309f8e37bce4e44b70aad88d24de767436963c60
Ali Najmaldin <ali.najmaldin@gmail.com>
2020-05-25 13:05:59 -0400

    just put flashflow at bottum so no overflow

--- src/components/SubFooter.js
+++ src/components/SubFooter.js
@@ -22,7 +22,7 @@ const SubFooter = props => (
             </ul>
             <ul>
               <li>
-                {/* <a href="https://www.flashflowdiagnostics.com">flashflowdiagnostics.com</a> */}
+                <a href="https://www.flashflowdiagnostics.com">flashflow</a>
               </li>
             </ul>
           </div>
